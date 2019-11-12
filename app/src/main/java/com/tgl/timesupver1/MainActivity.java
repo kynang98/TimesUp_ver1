77451aaf0026defe65823f9982451a4cc0e4770f
BrightFuture-APU <BrightFuture-APU@users.noreply.github.com>
2019-11-12 18:20:18 +0800

    Login Activity

--- app/src/main/java/com/tgl/timesupver1/MainActivity.java
+++ app/src/main/java/com/tgl/timesupver1/MainActivity.java
@@ -1,17 +1,80 @@
 package com.tgl.timesupver1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailID, password;
+    Button btnSignUp;
+    TextView tvSignIn;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, Invigilator_Home.class);
-        startActivity(intent);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailID = findViewById(R.id.signup_email);
+        password = findViewById(R.id.signup_pw);
+        btnSignUp = (Button) findViewById(R.id.signup_signup);
+        tvSignIn = (TextView) findViewById(R.id.signup_signin);
+        btnSignUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String email = emailID.getText().toString();
+                String pwd = password.getText().toString();
+                if(email.isEmpty()){
+                    emailID.setError("Please enter Email");
+                    emailID.requestFocus();
+                }
+                else if(pwd.isEmpty()){
+                    password.setError("Please enter password");
+                    password.requestFocus();
+                }
+                else if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Fields are empty!", Toast.LENGTH_SHORT).show();
+                }
+                else if(!(email.isEmpty()) && !(pwd.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Sign Up unsuccessful! Please try again.", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(MainActivity.this, Invigilator_Home.class));
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Unexpected error occurred, please restart your application.", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        tvSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
