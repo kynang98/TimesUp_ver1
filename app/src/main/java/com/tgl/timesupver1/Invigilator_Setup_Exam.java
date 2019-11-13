89a23b16b00d4da66317f6ba7f6b2e1b97480e66
Bright Future-APU <apuiothack4@gmail.com>
2019-11-13 22:48:19 +0800

    finished login UI

--- app/src/main/java/com/tgl/timesupver1/Invigilator_Setup_Exam.java
+++ app/src/main/java/com/tgl/timesupver1/Invigilator_Setup_Exam.java
@@ -17,17 +17,6 @@ public class Invigilator_Setup_Exam extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invigilator__setup__exam);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         Intent intent = getIntent();
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
