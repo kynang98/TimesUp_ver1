2b46c86522b2fab19840a1ea4a6079725ac6e6cf
Bright Future-APU <apuiothack4@gmail.com>
2019-11-22 22:28:54 +0800

    Setup Exam UI dONE

---
+++ app/src/main/java/com/tgl/timesupver1/TimesUp.kt
@@ -0,0 +1,12 @@
+package com.tgl.timesupver1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TimesUp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_times_up)
+    }
+}
