4a3bfb0d5d720263a7b309aede7e72849e5455b0
Bright Future-APU <apuiothack4@gmail.com>
2019-11-22 22:23:08 +0800

    Setup Exam UI dONE

---
+++ app/src/main/java/com/tgl/timesupver1/Success.kt
@@ -0,0 +1,12 @@
+package com.tgl.timesupver1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Success : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success)
+    }
+}
